8b96162ea75ac3b784e48479a34b98c2eef55564
skywind3000 <skywind3000@163.com>
2016-12-15 03:43:44 +0800

    new init

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 
 
-;(setq config-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-(setq config-home (file-name-directory load-file-name))
+(setq config-home (file-name-directory 
+         (or (buffer-file-name) load-file-name)))
 
 (setq config-lisp (concat config-home "lisp"))
 (setq config-lib (concat config-home "lib"))
